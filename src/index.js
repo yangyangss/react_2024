7608008a91c358361cbf1b7f2a051490e2f372ec
yangyangss <chevy.yang@gmail.com>
2024-03-23 23:46:32 +0800

    add example react js file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM  from "react-dom/client";
+import App from "./App";
+import { element } from "prop-types";
+
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(element);
